e609361fb5cb775d45c4413756ac3a44f7206c5b
Caroline Carvalho <carvalhocaroline111@gmail.com>
2020-03-26 10:10:49 -0300

    Creates a 404 route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home";
+import store from "@/store";
 
 Vue.use(VueRouter);
 
@@ -11,7 +12,7 @@ const linkExactActiveClass = "vue-school-active-class";
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
     props: true
   },
@@ -35,7 +36,25 @@ const routes = [
           );
         }
       }
-    ]
+    ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(
+        destination => destination.slug === to.params.slug
+      );
+      if (exists) {
+        next();
+      } else {
+        next({ name: "notFound" });
+      }
+    }
+  },
+  {
+    path: "404",
+    alias: "*",
+    name: "notFound",
+    component: function() {
+      return import(/* webpackChunkName: "notFound" */ "../views/NotFound");
+    }
   }
 ];
 
